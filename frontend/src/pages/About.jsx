75861a73535805620206475f52a6e08d378c2ec3
elijohnsen <eljo1@videndjurs.net>
2024-09-20 04:12:43 +0200

    prettier-plugin-tailwindcss

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <section>
-      <div className="mycontainer">About </div>
+      <div className="mycontainer ">About </div>
     </section>
   );
 };
